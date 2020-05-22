95869db9b13b2586bec36c3f8480b457c3cc7959
Mauricio Céspedes <mcespedes99@gmail.com>
2020-05-22 16:17:01 -0600

    Inicialización de GitHub

---
+++ B71986.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Tarea #2. IE0405 - Modelos Probabilísticos de Señales y Sistemas.
+Empezada el Viernes 22 de Mayo 14:34 2020
+
+@author: Mauricio Céspedes Tenorio.
+Carné: B71986
+"""
