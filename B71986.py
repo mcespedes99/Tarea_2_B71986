c3b04f7b63225da538ba91ebfb54080c79ff38d5
Mauricio Céspedes <mcespedes99@gmail.com>
2020-05-23 17:01:19 -0600

    Calculo de probabilidades en el intervalo [19,86]

--- B71986.py
+++ B71986.py
@@ -31,3 +31,19 @@ pdf_fit = rayleigh.pdf(x,loc=parametros[0],scale=parametros[1])
 plt.plot(x,pdf_fit,'r-')
 #Comando para abrir las gráficas:
 plt.show()
+
+"""Encontrar la probabilidad en el intervalo [19, 86] en el modelo encontrado y contrastarlo con la frecuencia relativa de los elementos de datos.csv que están en realidad en ese mismo intervalo."""
+#Probabilidad con la curva encontrada: resta de la cdf en 86 y la cdf en 19 con los datos de mejor ajuste
+cdf_19y86 = rayleigh.cdf(86,loc=parametros[0],scale=parametros[1])-rayleigh.cdf(19,loc=parametros[0],scale=parametros[1])
+#Probabilidad con frecuencia relativa:
+#i) Primero se hace un DataFrame con los datos en el intevalo [19, 86]:
+df_entre_19y86 = df[(df[0]<=86) & (df[0]>=19)]
+#ii) Cantidad de datos totales del csv:
+N_total = len(df)
+#iii) Cantidad de datos totales del csv entre 19 y 86:
+N_entre_19y86 = len(df_entre_19y86)
+#iv) Frecuencia relativa (probabilidad en el intervalo [19,86]):
+frec_rel_19y86 = N_entre_19y86/N_total
+#Error de la probabilidad obtenida con el modelo:
+error = abs(cdf_19y86-frec_rel_19y86)/frec_rel_19y86
+print("La probabilidad en el intervalo [19,86] con el modelo dato es de: "+"{:.4f}".format(cdf_19y86*100)+"%.\nLa probabilidad obtenida con frecuencia relativa es de: "+"{:.4f}".format(frec_rel_19y86*100)+"%.\nEsto representa un error del ""{:.4f}".format(error*100)+"%, el cual es muy bajo.")
