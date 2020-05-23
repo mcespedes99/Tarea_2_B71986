5b7daaa70b72536c7e9a4d1ec1ca9518ed71be09
Mauricio Céspedes <mcespedes99@gmail.com>
2020-05-22 20:18:27 -0600

    histograma no normalizado

--- B71986.py
+++ B71986.py
@@ -6,3 +6,10 @@ Empezada el Viernes 22 de Mayo 14:34 2020
 @author: Mauricio Céspedes Tenorio.
 Carné: B71986
 """
+import matplotlib.pyplot as plt
+import pandas as pd
+
+df = pd.read_csv('datos.csv',header= None)
+a = df.to_numpy()
+plt.hist(a, bins=[0,5,10,15,20,25,30,35,40,45,50,55,60,65,70,75,80,85,90,95,100])
+plt.show()
